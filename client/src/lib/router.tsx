29e0c0082678703175f3860e2159fb5f11fa064f
Alyona-art <alena.artemeva.2002.15@gmail.com>
2023-11-11 00:21:04 +0400

    login and sign up

--- client/src/lib/router.tsx
+++ client/src/lib/router.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 import { createBrowserRouter } from 'react-router-dom';
 import LoginPage from '~/pages/login';
+import SignUp from '~/pages/signup';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
   },
 ]);
 
